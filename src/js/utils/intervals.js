91bdab27655a14791e4f1134ff667d2aceed84ba
ajv <andreasvirkus@gmail.com>
2018-10-25 16:46:20 +0300

    Fix reduce warning for empty arrays

--- src/js/utils/intervals.js
+++ src/js/utils/intervals.js
@@ -212,7 +212,7 @@ export function isInRange2D(coord, minCoord, maxCoord) {
 export function getClosestInArray(goal, arr, index = false) {
 	let closest = arr.reduce(function(prev, curr) {
 		return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
-	});
+	}, []);
 
 	return index ? arr.indexOf(closest) : closest;
 }
