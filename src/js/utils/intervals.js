64116e5e5b625f1008ebf6bbbd9c3d55ecb5cc55
nocord <53902539+nocord@users.noreply.github.com>
2019-08-13 04:34:34 +0600

    change some var to let , and some let to const

--- src/js/utils/intervals.js
+++ src/js/utils/intervals.js
@@ -11,22 +11,22 @@ function normalize(x) {
 	if(isNaN(x)) {
 		return {mantissa: -6755399441055744, exponent: 972};
 	}
-	var sig = x > 0 ? 1 : -1;
+	const sig = x > 0 ? 1 : -1;
 	if(!isFinite(x)) {
 		return {mantissa: sig * 4503599627370496, exponent: 972};
 	}
 
 	x = Math.abs(x);
-	var exp = Math.floor(Math.log10(x));
-	var man = x/Math.pow(10, exp);
+	const exp = Math.floor(Math.log10(x));
+	const man = x/Math.pow(10, exp);
 
 	return [sig * man, exp];
 }
 
 function getChartRangeIntervals(max, min=0) {
 	let upperBound = Math.ceil(max);
-	let lowerBound = Math.floor(min);
-	let range = upperBound - lowerBound;
+	const lowerBound = Math.floor(min);
+    let range = upperBound - lowerBound;
 
 	let noOfParts = range;
 	let partSize = 1;
@@ -63,7 +63,7 @@ function getChartRangeIntervals(max, min=0) {
 
 function getChartIntervals(maxValue, minValue=0) {
 	let [normalMaxValue, exponent] = normalize(maxValue);
-	let normalMinValue = minValue ? minValue/Math.pow(10, exponent): 0;
+	const normalMinValue = minValue ? minValue/Math.pow(10, exponent): 0;
 
 	// Allow only 7 significant digits
 	normalMaxValue = normalMaxValue.toFixed(6);
@@ -79,8 +79,8 @@ export function calcChartIntervals(values, withMinimum=false) {
 	// Calculates best-fit y intervals from given values
 	// and returns the interval array
 
-	let maxValue = Math.max(...values);
-	let minValue = Math.min(...values);
+	const maxValue = Math.max(...values);
+	const minValue = Math.min(...values);
 
 	// Exponent to be used for pretty print
 	let exponent = 0, intervals = []; // eslint-disable-line no-unused-vars
@@ -92,7 +92,7 @@ export function calcChartIntervals(values, withMinimum=false) {
 
 		// Then unshift the negative values
 		let value = 0;
-		for(var i = 1; value < absMinValue; i++) {
+		for(let i = 1; value < absMinValue; i++) {
 			value += intervalSize;
 			intervals.unshift((-1) * value);
 		}
@@ -119,7 +119,7 @@ export function calcChartIntervals(values, withMinimum=false) {
 		// Because ceil() and floor() behave differently
 		// in those two regions
 
-		let absMinValue = Math.abs(minValue);
+		const absMinValue = Math.abs(minValue);
 
 		if(maxValue >= absMinValue) {
 			exponent = normalize(maxValue)[1];
@@ -127,7 +127,7 @@ export function calcChartIntervals(values, withMinimum=false) {
 		} else {
 			// Mirror: maxValue => absMinValue, then change sign
 			exponent = normalize(absMinValue)[1];
-			let posIntervals = getPositiveFirstIntervals(absMinValue, maxValue);
+			const posIntervals = getPositiveFirstIntervals(absMinValue, maxValue);
 			intervals = posIntervals.map(d => d * (-1));
 		}
 
@@ -139,8 +139,8 @@ export function calcChartIntervals(values, withMinimum=false) {
 		// Mirrored Case I:
 		// Work with positives, then reverse the sign and array
 
-		let pseudoMaxValue = Math.abs(minValue);
-		let pseudoMinValue = Math.abs(maxValue);
+		const pseudoMaxValue = Math.abs(minValue);
+		const pseudoMinValue = Math.abs(maxValue);
 
 		exponent = normalize(pseudoMaxValue)[1];
 		if(!withMinimum) {
@@ -157,7 +157,7 @@ export function calcChartIntervals(values, withMinimum=false) {
 
 export function getZeroIndex(yPts) {
 	let zeroIndex;
-	let interval = getIntervalSize(yPts);
+	const interval = getIntervalSize(yPts);
 	if(yPts.indexOf(0) >= 0) {
 		// the range has a given zero
 		// zero-line on the chart
@@ -165,23 +165,23 @@ export function getZeroIndex(yPts) {
 	} else if(yPts[0] > 0) {
 		// Minimum value is positive
 		// zero-line is off the chart: below
-		let min = yPts[0];
+		const min = yPts[0];
 		zeroIndex = (-1) * min / interval;
 	} else {
 		// Maximum value is negative
 		// zero-line is off the chart: above
-		let max = yPts[yPts.length - 1];
+		const max = yPts[yPts.length - 1];
 		zeroIndex = (-1) * max / interval + (yPts.length - 1);
 	}
 	return zeroIndex;
 }
 
 export function getRealIntervals(max, noOfIntervals, min = 0, asc = 1) {
-	let range = max - min;
-	let part = range * 1.0 / noOfIntervals;
+	const range = max - min;
+	const part = range * 1.0 / noOfIntervals;
 	let intervals = [];
 
-	for(var i = 0; i <= noOfIntervals; i++) {
+	for(let i = 0; i <= noOfIntervals; i++) {
 		intervals.push(min + part * i);
 	}
 
@@ -210,7 +210,7 @@ export function isInRange2D(coord, minCoord, maxCoord) {
 }
 
 export function getClosestInArray(goal, arr, index = false) {
-	let closest = arr.reduce(function(prev, curr) {
+	const closest = arr.reduce((prev, curr) => {
 		return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
 	}, []);
 
@@ -221,13 +221,13 @@ export function calcDistribution(values, distributionSize) {
 	// Assume non-negative values,
 	// implying distribution minimum at zero
 
-	let dataMaxValue = Math.max(...values);
+	const dataMaxValue = Math.max(...values);
 
-	let distributionStep = 1 / (distributionSize - 1);
+	const distributionStep = 1 / (distributionSize - 1);
 	let distribution = [];
 
-	for(var i = 0; i < distributionSize; i++) {
-		let checkpoint = dataMaxValue * (distributionStep * i);
+	for(let i = 0; i < distributionSize; i++) {
+		const checkpoint = dataMaxValue * (distributionStep * i);
 		distribution.push(checkpoint);
 	}
 
