809bc2c43c292bf3d161765dd43eee943d2babb2
Shivam Mishra <scm.mymail@gmail.com>
2020-05-09 18:09:11 +0530

    chore: bump to 1.4.1

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import * as Charts from './chart';
 let frappe     = { };
 
 frappe.NAME    = 'Frappe Charts';
-frappe.VERSION = '1.4.0';
+frappe.VERSION = '1.4.1';
 
 frappe         = Object.assign({ }, frappe, Charts);
 
