ef5ab9c715272977205d88b7409292f53ca8ac11
Shivam Mishra <scm.mymail@gmail.com>
2020-05-09 18:08:26 +0530

    fix: legend render bug

--- src/js/charts/Heatmap.js
+++ src/js/charts/Heatmap.js
@@ -175,6 +175,7 @@ export default class Heatmap extends BaseChart {
 		this.legendArea.textContent = '';
 		let x = 0;
 		let y = ROW_HEIGHT;
+		let radius = this.rawChartArgs.radius || 0
 
 		let lessText = makeText('subdomain-name', x, y, 'Less',
 			{
@@ -187,7 +188,7 @@ export default class Heatmap extends BaseChart {
 
 		this.colors.slice(0, HEATMAP_DISTRIBUTION_SIZE).map((color, i) => {
 			const square = heatSquare('heatmap-legend-unit', x + (COL_WIDTH + 3) * i,
-				y, HEATMAP_SQUARE_SIZE, color);
+				y, HEATMAP_SQUARE_SIZE, radius, color);
 			this.legendArea.appendChild(square);
 		});
 
