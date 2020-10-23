c5de1fd06ca814182291a9f85a035ceb9bd6a6f9
Bart Van Houtte <>
2020-10-23 16:58:29 +0200

    - Fix tests

--- src/js/utils/test/colors.test.js
+++ src/js/utils/test/colors.test.js
@@ -3,12 +3,12 @@ const colors = require('../colors');
 
 describe('utils.colors', () => {
   it('should return #aaabac for RGB()', () => {
-    assert.equal(colors.getColor('rgb(170, 171, 172)', '#aaabac'));
+    assert.equal(colors.getColor('rgb(170, 171, 172)'), '#aaabac');
   });
   it('should return #ff5858 for the named color red', () => {
-    assert.equal(colors.getColor('red', '#ff5858d'));
+    assert.equal(colors.getColor('red'), '#ff5858d');
   });
   it('should return #1a5c29 for the hex color #1a5c29', () => {
-    assert.equal(colors.getColor('#1a5c29', '#1a5c29'));
+    assert.equal(colors.getColor('#1a5c29'), '#1a5c29');
   });
 });
