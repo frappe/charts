72d5507c6f36bf7b56aede048a810c1c90a3db3d
prssanna <prssud@gmail.com>
2021-03-09 16:56:18 +0530

    chore: bump to rc10

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import * as Charts from './chart';
 let frappe     = { };
 
 frappe.NAME    = 'Frappe Charts';
-frappe.VERSION = '2.0.0-rc5';
+frappe.VERSION = '2.0.0-rc10';
 
 frappe         = Object.assign({ }, frappe, Charts);
 
