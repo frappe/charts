a1eb743deefa9d599e4f358d69b6b5482435463c
Aravindha1234u <aravindha1234u@gmail.com>
2021-05-03 08:13:44 +0530

    Removed html file and Loop

--- src/js/utils/axis-chart-utils.js
+++ src/js/utils/axis-chart-utils.js
@@ -31,23 +31,8 @@ export function dataPrep(data, type) {
 			} else {
 				vals = fillArray(vals, datasetLength - vals.length, 0);
 			}
+			d.values = vals;
 		}
-
-		//XSS
-		try{
-			for(var i=0;i<datasetLength;i++){
-				var temp = data.datasets[i]['values'];
-				for(var j=0;j<temp.length;j++){
-					if(typeof(temp[j]) == "string"){
-						data.datasets[i]['values'][j] = String(temp[j]).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
-					}
-				}
-			}
-		}catch(e){
-		}
-
-		// Set labels
-		//
 
 		// Set type
 		if(!d.chartType ) {
