c18c076adc2123e2e9db39a19af4efdf32b4fa50
Shivam Mishra <scm.mymail@gmail.com>
2021-06-16 10:53:17 +0000

    chore: bump to rc19

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import * as Charts from './chart';
 let frappe     = { };
 
 frappe.NAME    = 'Frappe Charts';
-frappe.VERSION = '2.0.0-rc18';
+frappe.VERSION = '2.0.0-rc19';
 
 frappe         = Object.assign({ }, frappe, Charts);
 
