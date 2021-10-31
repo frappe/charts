982f121a2bdfd40f82e832b5989683f873b337d3
Juga Paazmaya <paazmaya@yahoo.com>
2021-10-31 21:53:41 +0200

    Update all dependencies, fresh Node.js versions at Travis, and fix one test typo

--- src/js/utils/test/colors.test.js
+++ src/js/utils/test/colors.test.js
@@ -6,7 +6,7 @@ describe('utils.colors', () => {
     assert.equal(colors.getColor('rgb(170, 171, 172)'), '#aaabac');
   });
   it('should return #ff5858 for the named color red', () => {
-    assert.equal(colors.getColor('red'), '#ff5858d');
+    assert.equal(colors.getColor('red'), '#ff5858');
   });
   it('should return #1a5c29 for the hex color #1a5c29', () => {
     assert.equal(colors.getColor('#1a5c29'), '#1a5c29');
